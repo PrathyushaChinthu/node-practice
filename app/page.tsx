384f7d1e05ef24f339c6cae36dc1a30717519fca
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-07 23:13:40 +0530

    css module

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import ProductCard from "./users/components/ProductCard";
+import ProductCard from "./users/components/ProductList/ProductCard";
 
 export default function Home() {
   return (
